48c5c9ec2f469ba7cc30ec315ea04a3de7d5e133
CarMurillo <164522347+CarMurillo@users.noreply.github.com>
2025-03-28 16:13:33 -0500

    Update vite.config.js55

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['florescumple.onrender.com']
+    host: '0.0.0.0',  // Permite conexiones desde cualquier IP
+    port: process.env.PORT || 5173,
+    allowedHosts: ['florescumple.onrender.com']  // Asegura que Render pueda acceder
   }
 });
